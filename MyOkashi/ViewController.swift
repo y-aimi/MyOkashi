31ea445e04deb59918e32531818eea5c75120502
y-aimi <yoshiki0885@gmail.com>
2020-03-09 16:00:41 +0900

    Initial Commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by 相見佳輝 on 2020/03/09.
+//  Copyright © 2020 相見佳輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
